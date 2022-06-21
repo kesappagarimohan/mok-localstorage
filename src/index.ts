3c3f416f2d8d12509e42a4feef1e3333e5af2b5e
MohanKesappagari-careator <mohan.k@careator.com>
2022-06-21 22:53:48 +0530

    New

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-function getLocalStoargeItem() {}
+function getLocalStorageItem(name: string): string | object {
+  const item: any = localStorage.getItem(name);
+  const data = JSON.parse(item) ? JSON.parse(item) : item;
+  return item ? data : "No data";
+}
+
+function setLocalStorageItem(name: string, value: string | object): void {
+  const data: string =
+    typeof value === "object" ? JSON.stringify(value) : value;
+  localStorage.setItem(name, data);
+}
+
+function removeLocalStorage(name: string): void {
+  localStorage.removeItem(name);
+}
+
+export { getLocalStorageItem, setLocalStorageItem, removeLocalStorage };
